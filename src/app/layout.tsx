220a51f278da545ce1c10dee8510a434c1934b59
Shourya Pal <palshourya897@gmail.com>
2024-01-30 14:51:08 +0530

    User Profile Menu and Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toaster'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from "next/font/google"
@@ -12,15 +13,21 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
+  authmodal
 }: {
   children: React.ReactNode
+  authmodal: React.ReactNode
 }) {
   return (
     <html lang='en' className={cn('bg-white text-slate-500 antialiased light', inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        <Navbar/>
-        <div className='container max-w-7xl mx-auto h-full pt-12'></div>
-        {children}
+        {/* @ts-expect-error server component */}
+        <Navbar />
+        {authmodal}
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+        <Toaster/>
       </body>
     </html>
   )
